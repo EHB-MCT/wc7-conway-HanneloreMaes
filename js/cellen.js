8df8ba2ee62edfdfaa613b92593f003fc5c4760f
HanneloreMaes <71761907+HanneloreMaes@users.noreply.github.com>
2021-04-01 11:14:00 +0200

    Start

---
+++ js/cellen.js
@@ -0,0 +1,10 @@
+"use strict";
+
+class Cell{
+    constructor(){
+        this.x = xPos;
+        this.y = yPos;
+        this.state = state;
+    }
+}
+export default Cell;
--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
 "use strict";
 
+class Game {
+    constructor(){
+        
+    }
+}
 
 function init() {
 
