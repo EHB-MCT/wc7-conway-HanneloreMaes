5d8a5258e2ac55eb0ac1320bd9fc42be45dcb224
HanneloreMaes <71761907+HanneloreMaes@users.noreply.github.com>
2021-04-21 12:01:24 +0200

    Toevoegen van Update bij cellen

--- js/cellen.js
+++ js/cellen.js
@@ -1,10 +1,21 @@
 "use strict";
 
 class Cell{
-    constructor(){
-        this.x = xPos;
-        this.y = yPos;
-        this.state = state;
+    constructor(x,y){
+        this._x = x;
+        this._y = y;
+        this._state = Math.floor(Math.random()*2);
+        this._element = document.createElement("div");
+        this._element.className = "cell";
+    }
+
+    update(cells){
+        cells.filter((c) => {
+            return c._x - this._x == 1 ||
+                c._x - this._x == -1 ||
+                c._y - this._y == 1 ||
+                c._y - this._y == -1
+        });
     }
 }
 export default Cell;
--- js/index.js
+++ js/index.js
@@ -1,10 +1,28 @@
 "use strict";
 
+import Cell from './cellen.js';
+
 class Game {
     constructor(){
-        
+        this.cells = [];
+
+        for (let i =0; i<20; i++){
+            for (let j =0; j<20; j++){
+                // toevoegen van cellen
+                const c = new Cell (i,j);   /* i = als x-waarde    j = als y-waarde */
+                this.cells.push(c);
+                
+            }
+        }
+    }
+    
+    update(){
+        this._cells.ForEach((c) => {
+    
+        });
     }
 }
+
 
 function init() {
 
